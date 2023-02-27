de156ecf5f4f18e4a6d80a6fe06d12095809a076
Knif-Daniel <58825956+Knif-Daniel@users.noreply.github.com>
2023-02-27 22:27:03 +0100

    Version 5

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 var GHPATH = 'https://knif-daniel.github.io/DeckOfCardsWorkout/';
  
 // Choose a different app prefix name
-var APP_PREFIX = 'gppwa_';
+var APP_PREFIX = 'DoCW';
  
 // The version of the cache. Every time you change any of the files
 // you need to change this version (version_01, version_02…). 
