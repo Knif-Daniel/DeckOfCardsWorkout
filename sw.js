ec621a12d536969060bc936ce1a5e8bc08042d2c
Knif-Daniel <58825956+Knif-Daniel@users.noreply.github.com>
2023-03-06 17:13:55 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -17,3 +17,28 @@ var URLS = [
   `${GHPATH}/index.html`,
   `${GHPATH}/WorkofCards.js`
 ]
+
+let currentPath = self.location.href.replace("/sw.js", "");
+
+const cacheName = "WoCW"
+const assets = [
+    "/",
+    "/index.html",
+    "/WorkofCards.js",
+].map(url => currentPath + url)
+
+self.addEventListener("install", installEvent => {
+    installEvent.waitUntil(
+        caches.open(cacheName).then(cache => {
+            cache.addAll(assets)
+        })
+    )
+});
+
+self.addEventListener("fetch", fetchEvent => {
+    fetchEvent.respondWith(
+        caches.match(fetchEvent.request).then(res => {
+            return res || fetch(fetchEvent.request)
+        })
+    )
+})
