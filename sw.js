9a26db871164df838fee6926595d05398fec434d
Knif-Daniel <58825956+Knif-Daniel@users.noreply.github.com>
2023-03-06 17:14:10 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,3 @@
-// Change this to your repository name
-var GHPATH = '/DeckOfCardsWorkout';
- 
-// Choose a different app prefix name
-var APP_PREFIX = 'DoCW';
- 
-// The version of the cache. Every time you change any of the files
-// you need to change this version (version_01, version_02…). 
-// If you don't change the version, the service worker will give your
-// users the old files!
-var VERSION = 'version_11';
- 
-// The files to make available for offline use. make sure to add 
-// others to this list
-var URLS = [    
-  `${GHPATH}/`,
-  `${GHPATH}/index.html`,
-  `${GHPATH}/WorkofCards.js`
-]
 
 let currentPath = self.location.href.replace("/sw.js", "");
 
